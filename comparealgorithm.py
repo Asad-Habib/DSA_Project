774f882766cce05ded95dca24b14fae5d50affea
Rija Hasan Abidi <95442492+rijahasan@users.noreply.github.com>
2022-05-06 22:58:29 +0500

    readme

--- comparealgorithm.py
+++ comparealgorithm.py
@@ -1,7 +1,6 @@
 from Edges import G1,positions1,edges1, addNodes
 from helper import enqueue,dequeue,get_neighbors,addNodes1
-import math
-import time
+import math,time
 def dijkstra(graph,s,e):
   PQ=[]
   enqueue(PQ,(s,0))
@@ -35,7 +34,6 @@ djisktratime=time.time()
 output=(dijkstra(G1,'Gate 2', 'ATM'))
 djisktratime=time.time()-djisktratime
 print("Djisktra execution time:",djisktratime)
-
 stroutput=''
 c=1
 for a in range(len(output)):
@@ -49,7 +47,7 @@ for a in range(len(output)):
         stroutput+='Checkpoint '+str(c)+': '+output[a][1]+'\n'
         c+=1
 print(stroutput)
-
+#This heuristic function approximates the heuristic value by the straight line distance formula 
 def h1(n,stop,positions):
     for i in positions:
         if i[0]==n:
@@ -114,15 +112,6 @@ astar_time=time.time()-astar_time
 print("A* execution time:",astar_time)
 stroutput=''
 c=1
-# for a in range(len(output)):
-#     if a==0:
-#         stroutput+='Here is your path followed by direction bearings!\nCurrent Location: '+output[a][0]+' '+output[a][1]+'\n'
-#     elif a==len(output)-1:
-#         stroutput+='Destination: '+output[a][0]+' '+output[a][1]+'\n'
-#     else:
-#         stroutput+='Checkpoint '+str(c)+': '+output[a][0]+' '+output[a][1]+'\n'
-#         c+=1
-# print(stroutput)
 for a in range(len(output)):
     if a==0:
         stroutput+='Current Location: '+output[a]+'\n'
@@ -140,24 +129,20 @@ def h(n,edges,start=0,end=0):
     H=addNodes(edges,1)
     return H[n]
 def a_star_algorithm(G, start, stop, edges,B=False):
-#open_lst: a list of nodes which have been visited, but who's neighbours haven't all been always inspected, It starts off with the start node
-#closed_lst: a list of nodes which have been visited and who's neighbors have been always inspected
     open_lst = set([start])
     closed_lst = set([])
-    # the default value is +infinity
-    dist={}#dist has present distances from start to all other nodes
+    dist={}
     dist[start]=0
-    path={}# path contains an adjac mapping of all nodes
+    path={}
     path[start]=start
     while len(open_lst) > 0:
         n=None
-        for lowfv in open_lst:# it will find a node with the lowest value of f()
+        for lowfv in open_lst:
             if n==None or dist[lowfv]+h(lowfv,edges)<dist[n]+h(n,edges):
                 n=lowfv
         if n==None:
             print('Path does not exist!')
             return None
-        # if the current node is the stop then we start again from start
         if n==stop:
             reconst_path=[]
             while path[n]!=n:
@@ -171,16 +156,12 @@ def a_star_algorithm(G, start, stop, edges,B=False):
                         if reconst_path[a]==edges[b][0]:
                             reconst_path[a]=(reconst_path[a],str(edges[b][3]))
             return reconst_path
-        # for all the neighbors of the current node do
         for a in get_neighbors(G, n):
             m,weight=a[0],a[1]
-# if the current node is not presentin both open_lst and closed_lst add it to open_lst and note n as it's path
             if m not in open_lst and m not in closed_lst:
                 open_lst.add(m)
                 path[m]=n
                 dist[m] = dist[n] + weight
-# otherwise, check if it's quicker to first visit n, then m and if it is, update path data and poo data
-# and if the node was in the closed_lst, move it to open_lst
             else:
                 if dist[m]>dist[n]+weight:
                     dist[m] = dist[n] + weight
@@ -188,7 +169,6 @@ def a_star_algorithm(G, start, stop, edges,B=False):
                     if m in closed_lst:
                         closed_lst.remove(m)
                         open_lst.add(m)
-#remove n from the open_lst, and add it to closed_lst because all of his neighbors were inspected
         open_lst.remove(n)
         closed_lst.add(n)
     print('Path does not exist!')
@@ -208,15 +188,3 @@ for a in range(len(output)):
         stroutput+='Checkpoint '+str(c)+': '+output[a]+'\n'
         c+=1
 print(stroutput)
-# output=a_star_algorithm(G,'Gate 2', 'Tapal 1',edges)
-# stroutput=''
-# c=1
-# for a in range(len(output)):
-#     if a==0:
-#         stroutput+='Here is your path followed by direction bearings!\nCurrent Location: '+output[a][0]+' '+output[a][1]+'\n'
-#     elif a==len(output)-1:
-#         stroutput+='Destination: '+output[a][0]+' '+output[a][1]+'\n'
-#     else:
-#         stroutput+='Checkpoint '+str(c)+': '+output[a][0]+' '+output[a][1]+'\n'
-#         c+=1
-# print(stroutput)
